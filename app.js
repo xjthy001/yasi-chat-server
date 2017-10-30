892123ac00018a1eaaa2701fbd1d4da88f781dc7
唐昊宇 <xjthy@qq.com>
2017-10-31 03:36:58 +0800

    新增聊天列表接口，收藏接口

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ let mongoPromise = mongoose.connect('mongodb://39.106.55.194:27017/test', { useM
 mongoose.Promise = global.Promise
 console.log('服务正在运行，端口3232')
 server.listen(3232)
-
 // 创建schema
 // 聊天数据库
 let chatSchema = mongoose.Schema({
@@ -64,6 +63,59 @@ let userSchema = mongoose.Schema({
 })
 let userModel = mongoose.model('user', userSchema)// 用户信息
 
+// 学习模块阅读
+let readingSchema = mongoose.Schema({
+  name: String,
+  content: String,
+  imgUrl: String,
+  des: String,
+  date: Number,
+  id: Number,
+  like: Number,
+  collect: Number,
+  index: Number
+})
+let readingModel = mongoose.model('reading', readingSchema)
+// 学习板块听力
+let listeningSchema = mongoose.Schema({
+  name: String,
+  content: String,
+  imgUrl: String,
+  des: String,
+  date: Number,
+  id: Number,
+  like: Number,
+  collect: Number,
+  index: Number
+})
+let listeningModel = mongoose.model('listening', listeningSchema)
+
+let infoSchema = mongoose.Schema({
+  name: String,
+  content: String,
+  imgUrl: String,
+  des: String,
+  date: Number,
+  id: Number,
+  like: Number,
+  collect: Number,
+  index: Number
+})
+let infoModel = mongoose.model('info', infoSchema)
+
+let recommendSchema = mongoose.Schema({
+  name: String,
+  imgUrl: String,
+  des: String,
+  date: Number,
+  id: Number,
+  like: Number,
+  collect: Number,
+  style: String
+})
+let recommendModel = mongoose.model('recommend', recommendSchema)
+
+// **************************************************
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html')
 })
@@ -102,7 +154,6 @@ io.on('connection', function (socket) {
         return chatModel.findOneAndUpdate({ phoneNum: from, with: to }, { phoneNum: from, with: to, chat: arr }, { upsert: true })
       })
       .catch((err) => {
-        console.error('未能操作成功1')
         console.error(err)
       })
 
@@ -120,7 +171,6 @@ io.on('connection', function (socket) {
         return chatModel.findOneAndUpdate({ phoneNum: to, with: from }, { phoneNum: to, with: from, chat: arr }, { upsert: true })
       })
       .catch((err) => {
-        console.error('未能操作成功2')
         console.error(err)
       })
     let toSocket = findUser(to)
@@ -140,7 +190,6 @@ io.on('connection', function (socket) {
 // 获取聊天记录接口
 app.get('/getchat', function (req, res) {
   res.append('Access-Control-Allow-Origin', '*')
-  console.log(req.query)
   if (!req.query || !req.query.pn || !req.query.wz) {
     res.sendStatus(403).end()
   } else {
@@ -154,4 +203,222 @@ app.get('/getchat', function (req, res) {
         res.sendStatus(500).end()
       })
   }
+})
+// 获取chatlist(供admin使用)
+app.get('/getchatlist', function (req, res) {
+  res.append('Access-Control-Allow-Origin', '*')
+  let userList, chatList
+  let arr = []
+  let flag = 0
+  Promise.all([userModel.find(), chatModel.find()])
+    .then((resolve) => {
+      userList = resolve[0]
+      chatList = resolve[1]
+      userList.forEach((val) => {
+        if (val.phoneNum == 10086) {
+          flag++
+          return
+        }
+        let phoneNum = val.phoneNum
+        let obj = {
+          phoneNum,
+          imgUrl: val.imgUrl,
+          user: val.user
+        }
+        chatModel.findOne({ phoneNum })
+          .then((data) => {
+            if (data) {
+              let index = data.chat.length - 1
+              obj.recentContent = data.chat[index].mes
+              obj.recentTime = data.chat[index].date
+              obj.contentItemNum = data.chat.length
+            }
+            flag++
+            arr.push(obj)
+            // 如果完成遍历
+            if (flag === userList.length) {
+              res.send(arr)
+            }
+          })
+          .catch((err) => {
+            console.error(err)
+          })
+      })
+      return arr
+    })
+})
+
+// 添加喜欢
+app.get('/getlike', function (req, res) {
+  res.append('Access-Control-Allow-Origin', '*')
+  const style = req.query.style
+  const id = req.query.key
+  const pn = req.query.pn
+  if (!style || !id || !pn) return
+  let like
+  let likeArray
+  let i
+  let promise1 = userModel.findOne({ phoneNum: pn })
+    .then((data) => {
+      if (!data) return
+      console.log(data)
+      likeArray = data.like || []
+      likeArray.forEach((val, index) => {
+        if (val.id === id) {
+          i = index
+        }
+      })
+      if (!i) {
+        likeArray.push({
+          id: id,
+          type: style
+        })
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(500).end()
+    })
+  let issueModel
+  if (style === 'info') issueModel = infoModel
+  if (style === 'reading') issueModel = readingModel
+  if (style === 'listening') issueModel = listeningModel
+  let promise2 = issueModel.findOne({ id })
+    .then((data) => {
+      if (!data) return
+      like = data.like + 1
+      console.log('addlike', like)
+      console.log(data)
+      return data
+    })
+    .then((data) => {
+      // recommendModel.findOneAndUpdate({ id }, {
+      //   '$inc': { 'like': 1 }
+      // })
+      // issueModel.findOneAndUpdate({ id }, {
+      //   '$inc': { 'like': 1 }
+      // })
+      return data
+    })
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(500).end()
+    })
+
+  Promise.all([promise1, promise2])
+    .then((resolve) => {
+      let p1, p2, p3
+      if (!i) {
+        // console.log(resolve[1])
+        likeArray[likeArray.length - 1].detail = Object.assign({}, resolve[1]._doc, { style })
+        // console.log('style', style)
+        console.log('style', likeArray[likeArray.length - 1].detail)
+        p1 = userModel.findOneAndUpdate({ phoneNum: pn }, {
+          $set: {
+            like: likeArray
+          }
+        })
+      } else {
+        console.log('存在i，不更新')
+        p1 = Promise.resolve()
+      }
+      p2 = recommendModel.findOneAndUpdate({ id }, {
+        $set: {
+          like: like
+        }
+      })
+      p3 = issueModel.findOneAndUpdate({ id }, {
+        $set: {
+          like: like
+        }
+      })
+      Promise.all([p1, p2, p3])
+        .then((resolve) => {
+          res.sendStatus(200)
+        })
+    })
+})
+
+// 取消喜欢
+app.get('/getdislike', function (req, res) {
+  console.log('getdislike')
+  res.append('Access-Control-Allow-Origin', '*')
+  const style = req.query.style
+  const id = req.query.id
+  const pn = req.query.pn
+  if (!style || !id || !pn) return
+  let like
+  let likeArray
+  let promise1 = userModel.findOne({ phoneNum: pn })
+    .then((data) => {
+      if (!data) return
+      likeArray = data.like || []
+      let i = null
+      likeArray.forEach((val, index) => {
+        if (val.id === id) {
+          i = index
+        }
+      })
+      if (i) {
+        console.log('目前存在数组中')
+        console.log(likeArray.length)
+        likeArray.splice(i, 1)
+        console.log('已删除')
+        console.log(likeArray.length)
+      }
+      console.log('finally', likeArray.length)
+      userModel.findOneAndUpdate({ phoneNum: pn }, {
+        $set: {
+          like: likeArray
+        }
+      })
+    })
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(500).end()
+    })
+  let issueModel
+  if (style === 'info') issueModel = infoModel
+  if (style === 'reading') issueModel = readingModel
+  if (style === 'listening') issueModel = listeningModel
+  let promise2 = issueModel.findOne({ id })
+    .then((data) => {
+      if (!data) return
+      console.log(data)
+      like = Math.max(0, data.like - 1)
+      return data
+    })
+    .then((data) => {
+      return data
+    })
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(500).end()
+    })
+  Promise.all([promise1, promise2])
+    .then(() => {
+      console.log('finally like', like)
+      const p1 = recommendModel.findOneAndUpdate({ id }, {
+        $set: {
+          like: like
+        }
+      })
+      const p2 = issueModel.findOneAndUpdate({ id }, {
+        $set: {
+          like: like
+        }
+      })
+      const p3 = userModel.findOneAndUpdate({ phoneNum: pn }, {
+        $set: {
+          like: likeArray
+        }
+      })
+      Promise.all([p1, p2, p3])
+        .then(() => {
+          res.sendStatus(200)
+        })
+        .catch(() => {
+          res.sendStatus(500)
+        })
+    })
 })
